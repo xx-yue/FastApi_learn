54543e702a80513a0affcb54fcf7799a3c12a241
xiexinyue <1586552582@qq.com>
2026-07-05 10:30:48 +0800

    [cd16]--依赖注入

--- fastApi_first/15_19/15_.py
+++ fastApi_first/15_19/15_.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends  # 2. 导入 Depends
 
 app = FastAPI()
 
@@ -17,6 +17,25 @@ async def middleware1(request, call_next):
     response = await call_next(request)
     print("中间件1 end")
     return response
+
+# 16 分页参数逻辑共用： 新闻列表和用户列表
+# 1. 依赖项
+async def common_parameters(
+    skip: int = Query(0, ge=0),
+    limit: int = Query(10, le=60)
+):
+    return {"skip": skip, "limit": limit}
+
+
+# 3. 声明依赖项 → 依赖注入
+@app.get("/news/news_list")
+async def get_news_list(commons=Depends(common_parameters)):
+    return commons
+
+
+@app.get("/user/user_list")
+async def get_user_list(commons=Depends(common_parameters)):
+    return commons
 
 
 @app.get("/")
